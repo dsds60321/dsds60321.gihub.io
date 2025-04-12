4a311002bf142c33766929df468ec8a73c4e282c
dsds60321 <dsds60321@gmail.com>
2025-04-13 03:03:16 +0900

    Add GitHub Actions workflow for automatic deployment

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 import path from 'path';
 
 const nextConfig: NextConfig = {
-    output: 'export', // 정적 HTML 내보내기 설정
-    distDir: 'out', // 빌드 출력 디렉토리 설정
+    output: 'export',
+    distDir: 'out',
     images: {
-        unoptimized: true, // GitHub Pages에서는 Next.js 이미지 최적화가 작동하지 않음
+        unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/repository-name' : '', // 저장소 이름이 username.github.io가 아닌 경우 설정
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/repository-name' : '', // 저장소 이름이 username.github.io가 아닌 경우 설정
+    basePath: '',
+    assetPrefix: '',
     webpack: (config) => {
         config.resolve.alias['@data'] = path.join(__dirname, 'data');
         config.resolve.alias['@type'] = path.join(__dirname, 'type');

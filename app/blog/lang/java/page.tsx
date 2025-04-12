debf22d83bc4a416bedb3ca97d2502b1462e55fd
dsds60321 <dsds60321@gmail.com>
2025-04-13 03:58:19 +0900

    feat: 중간저장

--- app/blog/lang/java/page.tsx
+++ app/blog/lang/java/page.tsx
@@ -1,13 +1,13 @@
 import { BlogList } from '@components/Blog/BlogList';
-import { reactPosts } from '@data/blog/react/react';
+import { kafkaPosts } from '@data/blog/kafka/kafka';
 import Layout from "@components/layout/Layout";
 
-export default function ReactPage() {
+export default function KafkaPage() {
     return (
         <Layout>
             <div className="container mx-auto px-4 py-12">
                 <h1 className="text-4xl font-bold mb-8">블로그</h1>
-                <BlogList posts={reactPosts} className="md:grid-cols-2 lg:grid-cols-3" />
+                <BlogList posts={kafkaPosts} className="md:grid-cols-2 lg:grid-cols-3" />
             </div>
         </Layout>
     );
